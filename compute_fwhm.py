3c8c454f3f04b3c35503715c66782240f7c278f1
Sean Dillon <seandillon48@gmail.com>
2024-09-07 15:24:19 -0500

    added files for homework, now on question 6

---
+++ compute_fwhm.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  7 13:20:55 2024
+
+@author: physics
+"""
+from sys import argv
+
+
+def fwhm(lam, d_tel):
+    fwhm = 1.028 * 206265 * float(lam) * 1e-6 / float(d_tel)
+    return fwhm
+
+# script, lam, d_tel = argv
+
+# print('The FWHM is %f' %(fwhm(lam, d_tel)))
---
+++ compute_fwhm_revised.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  7 14:12:19 2024
+
+@author: physics
+"""
+
+from compute_fwhm import fwhm
+
+
+print(fwhm(1.6,8.2))

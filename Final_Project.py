97e2590e2b1712900d508f8afbd38bf391a921ec
[Sean Dillon] <[seandillon48@gmail.com]>
2024-11-18 11:57:58 -0600

    worked on final project

--- Final_Project.py
+++ Final_Project.py
@@ -8,3 +8,28 @@ Created on Mon Nov 11 10:31:24 2024
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.decomposition import PCA
+# data = np.loadtxt('pca_csv.csv', dtype=str)
+df = pd.read_csv('pca_csv.csv')
+
+print(df.columns)
+
+
+
+
+data = df.iloc[:,4:-1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+

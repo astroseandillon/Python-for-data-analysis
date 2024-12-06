b40bf74ea3c49cf7ab6b4adaf1c43ea7b8361e8b
Sean Dillon <sean.dillon@my.utsa.edu>
2024-12-05 23:15:23 -0600

    worked on project, added info to py file

--- Final_Project.py
+++ Final_Project.py
@@ -8,17 +8,46 @@ Created on Mon Nov 11 10:31:24 2024
 
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-from sklearn.decomposition import PCA
-# data = np.loadtxt('pca_csv.csv', dtype=str)
-df = pd.read_csv('pca_csv.csv')
+from astropy.io import ascii
 
-print(df.columns)
+data = ascii.read('pca_csv.csv')
 
 
 
 
-data = df.iloc[:,4:-1]
+
+
+
+# data.info
+
+# <Table length=136>
+#          name           dtype     class     n_bad
+# ---------------------- ------- ------------ -----
+#                   Name   str19       Column     0
+#                     RA   str11       Column     0
+#                    Dec   str12       Column     0
+#            Other names   str80 MaskedColumn     1
+#     Kinematic Age (yr)   int64 MaskedColumn    25
+#  Central Star Temp (K)   int64 MaskedColumn    42
+# Structure (Literature)   int64       Column     0
+# Aspect Ratio (Overall) float64       Column     0
+#    Opening Angle (Avg)    str5 MaskedColumn   100
+#      PG Mass (Solar M) float64 MaskedColumn    78
+#                 C(e-4) float64 MaskedColumn    83
+#                 O(e-4) float64 MaskedColumn    70
+#                    C/O float64 MaskedColumn    68
+# Metallicity [O/H](e-4) float64 MaskedColumn    52
+# Radial Velocity (km/s) float64       Column     0
+#            Magnititude float64 MaskedColumn    31
+#                    MAT float64 MaskedColumn    80
+#              Mass/Temp float64 MaskedColumn    79
+#                     MA   int64 MaskedColumn    78
+#                    A/T float64 MaskedColumn    46
+#                     TA   int64 MaskedColumn    47
+
+
+
+
 
 
 
